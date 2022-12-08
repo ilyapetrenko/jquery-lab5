e651cba631b59b938531e0d378c163676f743b39
ilyapetrenko <91661624+ilyapetrenko@users.noreply.github.com>
2022-12-09 00:50:12 +0200

    Add files via upload

---
+++ polzunok.js
@@ -0,0 +1,5 @@
+function fun1() {
+    var rng=document.getElementById('r1'); //rng - это Input
+      var div=document.getElementById('test'); // div - блок test
+    div.style.width=rng.value+'px';
+  }
